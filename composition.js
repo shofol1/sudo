462da8965ef3a89518388b3f40e7f0eba12fe7ea
shofol <18103156@iubat.edu>
2022-06-27 21:47:04 +0600

    composition

---
+++ composition.js
@@ -0,0 +1,14 @@
+function sum(a, b) {
+  return a + b;
+}
+function sub(a, b) {
+  return a - b;
+}
+function multy(a, b) {
+  return a * b;
+}
+
+const r1 = sum(2, 3);
+const r2 = sub(3, 2);
+r3 = multy(r1, r2);
+console.log(r3);
